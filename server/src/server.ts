609cb8669be0ede6608f38c21aeb48622948b248
Walter Marinho <waltersmarinho@edu.unifil.br>
2020-06-06 12:30:07 -0300

    Dia #2 - Back end do app

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,38 +1,13 @@
-import express, { request, response } from "express";
+import express from "express";
+import cors from "cors";
+import path from "path";
+import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-const users = ["Diego", "Clayton", "Robson"];
-
-app.get("/users", (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(search))
-    : users;
-
-  response.json(filteredUsers);
-});
-
-app.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  return response.json(user);
-});
-
-app.post("/users", (request, response) => {
-  const data = request.body;
-
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return response.json(user);
-});
+app.use(routes);
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);

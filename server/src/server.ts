73ed27c3ecbdb11215db5b56e1fb306379905330
Walter Marinho <waltersmarinho@edu.unifil.br>
2020-06-05 23:01:48 -0300

    Explicação sobre Rotas, request params e knex

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,38 @@
-import express from "express";
+import express, { request, response } from "express";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("Listagem de usuários");
+app.use(express.json());
 
-  response.json(["Diego", "Clayton", "Robson"]);
+const users = ["Diego", "Clayton", "Robson"];
+
+app.get("/users", (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
+
+  response.json(filteredUsers);
+});
+
+app.get("/users/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post("/users", (request, response) => {
+  const data = request.body;
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+
+  return response.json(user);
 });
 
 app.listen(3333);
